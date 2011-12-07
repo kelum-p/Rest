b65fcf59bf1ded219930f46f13b37e3ee5f671fc
Kelum <kelum.peiris@polarmobile.com>
2011-12-06 22:54:15 -0500

    Resharpned JSON.cs

--- src/CocoB/Rest/Rest.WindowsPhone/Core/Parsers/JSON.cs
+++ src/CocoB/Rest/Rest.WindowsPhone/Core/Parsers/JSON.cs
@@ -7,110 +7,126 @@ using System.Text;
 namespace CocoB.Rest.WindowsPhone.Core.Parsers
 {
 	/// <summary>
-	/// This class encodes and decodes JSON strings.
-	/// Spec. details, see http://www.json.org/
+	///   Source obtained from: 
+	///     http://techblog.procurios.nl/k/news/view/14605/14863/How-do-I-write-my-own-parser-for-JSON.html
 	/// 
-	/// JSON uses Arrays and Objects. These correspond here to the datatypes ArrayList and Hashtable.
-	/// All numbers are parsed to doubles.
+	///   This class encodes and decodes JSON strings.
+	///   Spec. details, see http://www.json.org/
+	/// 
+	///   JSON uses Arrays and Objects. These correspond here to the datatypes ArrayList and Hashtable.
+	///   All numbers are parsed to doubles.
 	/// </summary>
 	public class JSON
 	{
-	    private const int TOKEN_NONE = 0;
-	    private const int TOKEN_CURLY_OPEN = 1;
-	    private const int TOKEN_CURLY_CLOSE = 2;
-	    private const int TOKEN_SQUARED_OPEN = 3;
-	    private const int TOKEN_SQUARED_CLOSE = 4;
-	    private const int TOKEN_COLON = 5;
-	    private const int TOKEN_COMMA = 6;
-	    private const int TOKEN_STRING = 7;
-	    private const int TOKEN_NUMBER = 8;
-	    private const int TOKEN_TRUE = 9;
-	    private const int TOKEN_FALSE = 10;
-	    private const int TOKEN_NULL = 11;
+		private const int TOKEN_NONE = 0;
+		private const int TOKEN_CURLY_OPEN = 1;
+		private const int TOKEN_CURLY_CLOSE = 2;
+		private const int TOKEN_SQUARED_OPEN = 3;
+		private const int TOKEN_SQUARED_CLOSE = 4;
+		private const int TOKEN_COLON = 5;
+		private const int TOKEN_COMMA = 6;
+		private const int TOKEN_STRING = 7;
+		private const int TOKEN_NUMBER = 8;
+		private const int TOKEN_TRUE = 9;
+		private const int TOKEN_FALSE = 10;
+		private const int TOKEN_NULL = 11;
 
 		private const int BUILDER_CAPACITY = 2000;
 
 		/// <summary>
-		/// Parses the string json into a value
+		///   Parses the string json into a value
 		/// </summary>
-		/// <param name="json">A JSON string.</param>
+		/// <param name = "json">A JSON string.</param>
 		/// <returns>An ArrayList, a Hashtable, a double, a string, null, true, or false</returns>
 		public static object JsonDecode(string json)
 		{
-			bool success = true;
-			
+			var success = true;
+
 			return JsonDecode(json, ref success);
 		}
 
 		/// <summary>
-		/// Parses the string json into a value; and fills 'success' with the successfullness of the parse.
+		///   Parses the string json into a value; and fills 'success' with the successfullness of the parse.
 		/// </summary>
-		/// <param name="json">A JSON string.</param>
-		/// <param name="success">Successful parse?</param>
+		/// <param name = "json">A JSON string.</param>
+		/// <param name = "success">Successful parse?</param>
 		/// <returns>An ArrayList, a Hashtable, a double, a string, null, true, or false</returns>
 		public static object JsonDecode(string json, ref bool success)
 		{
 			success = true;
-			if (json != null) {
-				char[] charArray = json.ToCharArray();
-				int index = 0;
-				object value = ParseValue(charArray, ref index, ref success);
+			if (json != null)
+			{
+				var charArray = json.ToCharArray();
+				var index = 0;
+				var value = ParseValue(charArray, ref index, ref success);
 				return value;
-			} else {
+			}
+			else
+			{
 				return null;
 			}
 		}
-	
+
 		/// <summary>
-		/// Converts a Hashtable / ArrayList object into a JSON string
+		///   Converts a Hashtable / ArrayList object into a JSON string
 		/// </summary>
-		/// <param name="json">A Hashtable / ArrayList</param>
+		/// <param name = "json">A Hashtable / ArrayList</param>
 		/// <returns>A JSON encoded string, or null if object 'json' is not serializable</returns>
 		public static string JsonEncode(object json)
 		{
-			StringBuilder builder = new StringBuilder(BUILDER_CAPACITY);
-			bool success = SerializeValue(json, builder);
+			var builder = new StringBuilder(BUILDER_CAPACITY);
+			var success = SerializeValue(json, builder);
 			return (success ? builder.ToString() : null);
 		}
 
 		protected static Dictionary<string, object> ParseObject(char[] json, ref int index, ref bool success)
 		{
-            var table = new Dictionary<string, object>();
+			var table = new Dictionary<string, object>();
 			int token;
 
 			// {
 			NextToken(json, ref index);
 
-			bool done = false;
-			while (!done) {
+			var done = false;
+			while (!done)
+			{
 				token = LookAhead(json, index);
-				if (token == JSON.TOKEN_NONE) {
+				if (token == TOKEN_NONE)
+				{
 					success = false;
 					return null;
-				} else if (token == JSON.TOKEN_COMMA) {
+				}
+				else if (token == TOKEN_COMMA)
+				{
 					NextToken(json, ref index);
-				} else if (token == JSON.TOKEN_CURLY_CLOSE) {
+				}
+				else if (token == TOKEN_CURLY_CLOSE)
+				{
 					NextToken(json, ref index);
 					return table;
-				} else {
-
+				}
+				else
+				{
 					// name
-					string name = ParseString(json, ref index, ref success);
-					if (!success) {
+					var name = ParseString(json, ref index, ref success);
+					if (!success)
+					{
 						success = false;
 						return null;
 					}
 
 					// :
 					token = NextToken(json, ref index);
-					if (token != JSON.TOKEN_COLON) {
+					if (token != TOKEN_COLON)
+					{
 						success = false;
 						return null;
 					}
 
 					// value
-					object value = ParseValue(json, ref index, ref success);
-					if (!success) {
+					var value = ParseValue(json, ref index, ref success);
+					if (!success)
+					{
 						success = false;
 						return null;
 					}
@@ -124,25 +140,34 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 
 		protected static List<object> ParseArray(char[] json, ref int index, ref bool success)
 		{
-            var array = new List<object>();
+			var array = new List<object>();
 
 			// [
 			NextToken(json, ref index);
 
-			bool done = false;
-			while (!done) {
-				int token = LookAhead(json, index);
-				if (token == JSON.TOKEN_NONE) {
+			var done = false;
+			while (!done)
+			{
+				var token = LookAhead(json, index);
+				if (token == TOKEN_NONE)
+				{
 					success = false;
 					return null;
-				} else if (token == JSON.TOKEN_COMMA) {
+				}
+				else if (token == TOKEN_COMMA)
+				{
 					NextToken(json, ref index);
-				} else if (token == JSON.TOKEN_SQUARED_CLOSE) {
+				}
+				else if (token == TOKEN_SQUARED_CLOSE)
+				{
 					NextToken(json, ref index);
 					break;
-				} else {
-					object value = ParseValue(json, ref index, ref success);
-					if (!success) {
+				}
+				else
+				{
+					var value = ParseValue(json, ref index, ref success);
+					if (!success)
+					{
 						return null;
 					}
 
@@ -155,25 +180,26 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 
 		protected static object ParseValue(char[] json, ref int index, ref bool success)
 		{
-			switch (LookAhead(json, index)) {
-				case JSON.TOKEN_STRING:
+			switch (LookAhead(json, index))
+			{
+				case TOKEN_STRING:
 					return ParseString(json, ref index, ref success);
-				case JSON.TOKEN_NUMBER:
+				case TOKEN_NUMBER:
 					return ParseNumber(json, ref index, ref success);
-				case JSON.TOKEN_CURLY_OPEN:
+				case TOKEN_CURLY_OPEN:
 					return ParseObject(json, ref index, ref success);
-				case JSON.TOKEN_SQUARED_OPEN:
+				case TOKEN_SQUARED_OPEN:
 					return ParseArray(json, ref index, ref success);
-				case JSON.TOKEN_TRUE:
+				case TOKEN_TRUE:
 					NextToken(json, ref index);
 					return true;
-				case JSON.TOKEN_FALSE:
+				case TOKEN_FALSE:
 					NextToken(json, ref index);
 					return false;
-				case JSON.TOKEN_NULL:
+				case TOKEN_NULL:
 					NextToken(json, ref index);
 					return null;
-				case JSON.TOKEN_NONE:
+				case TOKEN_NONE:
 					break;
 			}
 
@@ -183,55 +209,76 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 
 		protected static string ParseString(char[] json, ref int index, ref bool success)
 		{
-			StringBuilder s = new StringBuilder(BUILDER_CAPACITY);
+			var s = new StringBuilder(BUILDER_CAPACITY);
 			char c;
 
 			EatWhitespace(json, ref index);
-			
+
 			// "
 			c = json[index++];
 
-			bool complete = false;
-			while (!complete) {
-
-				if (index == json.Length) {
+			var complete = false;
+			while (!complete)
+			{
+				if (index == json.Length)
+				{
 					break;
 				}
 
 				c = json[index++];
-				if (c == '"') {
+				if (c == '"')
+				{
 					complete = true;
 					break;
-				} else if (c == '\\') {
-
-					if (index == json.Length) {
+				}
+				else if (c == '\\')
+				{
+					if (index == json.Length)
+					{
 						break;
 					}
 					c = json[index++];
-					if (c == '"') {
+					if (c == '"')
+					{
 						s.Append('"');
-					} else if (c == '\\') {
+					}
+					else if (c == '\\')
+					{
 						s.Append('\\');
-					} else if (c == '/') {
+					}
+					else if (c == '/')
+					{
 						s.Append('/');
-					} else if (c == 'b') {
+					}
+					else if (c == 'b')
+					{
 						s.Append('\b');
-					} else if (c == 'f') {
+					}
+					else if (c == 'f')
+					{
 						s.Append('\f');
-					} else if (c == 'n') {
+					}
+					else if (c == 'n')
+					{
 						s.Append('\n');
-					} else if (c == 'r') {
+					}
+					else if (c == 'r')
+					{
 						s.Append('\r');
-					} else if (c == 't') {
+					}
+					else if (c == 't')
+					{
 						s.Append('\t');
 					}
-				} else {
+				}
+				else
+				{
 					s.Append(c);
 				}
-
 			}
 
-			if (!complete) {
+			if (!complete)
+			{
 				success = false;
 				return null;
 			}
@@ -243,11 +290,12 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 		{
 			EatWhitespace(json, ref index);
 
-			int lastIndex = GetLastIndexOfNumber(json, index);
-			int charLength = (lastIndex - index) + 1;
+			var lastIndex = GetLastIndexOfNumber(json, index);
+			var charLength = (lastIndex - index) + 1;
 
 			double number;
-			success = Double.TryParse(new string(json, index, charLength), NumberStyles.Any, CultureInfo.InvariantCulture, out number);
+			success = Double.TryParse(new string(json, index, charLength), NumberStyles.Any,
+									  CultureInfo.InvariantCulture, out number);
 
 			index = lastIndex + 1;
 			return number;
@@ -257,8 +305,10 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 		{
 			int lastIndex;
 
-			for (lastIndex = index; lastIndex < json.Length; lastIndex++) {
-				if ("0123456789+-.eE".IndexOf(json[lastIndex]) == -1) {
+			for (lastIndex = index; lastIndex < json.Length; lastIndex++)
+			{
+				if ("0123456789+-.eE".IndexOf(json[lastIndex]) == -1)
+				{
 					break;
 				}
 			}
@@ -267,8 +317,10 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 
 		protected static void EatWhitespace(char[] json, ref int index)
 		{
-			for (; index < json.Length; index++) {
-				if (" \t\n\r".IndexOf(json[index]) == -1) {
+			for (; index < json.Length; index++)
+			{
+				if (" \t\n\r".IndexOf(json[index]) == -1)
+				{
 					break;
 				}
 			}
@@ -276,7 +328,7 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 
 		protected static int LookAhead(char[] json, int index)
 		{
-			int saveIndex = index;
+			var saveIndex = index;
 			return NextToken(json, ref saveIndex);
 		}
 
@@ -284,115 +336,149 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 		{
 			EatWhitespace(json, ref index);
 
-			if (index == json.Length) {
-				return JSON.TOKEN_NONE;
+			if (index == json.Length)
+			{
+				return TOKEN_NONE;
 			}
-			
-			char c = json[index];
+
+			var c = json[index];
 			index++;
-			switch (c) {
+			switch (c)
+			{
 				case '{':
-					return JSON.TOKEN_CURLY_OPEN;
+					return TOKEN_CURLY_OPEN;
 				case '}':
-					return JSON.TOKEN_CURLY_CLOSE;
+					return TOKEN_CURLY_CLOSE;
 				case '[':
-					return JSON.TOKEN_SQUARED_OPEN;
+					return TOKEN_SQUARED_OPEN;
 				case ']':
-					return JSON.TOKEN_SQUARED_CLOSE;
+					return TOKEN_SQUARED_CLOSE;
 				case ',':
-					return JSON.TOKEN_COMMA;
+					return TOKEN_COMMA;
 				case '"':
-					return JSON.TOKEN_STRING;
-				case '0': case '1': case '2': case '3': case '4': 
-				case '5': case '6': case '7': case '8': case '9':
-				case '-': 
-					return JSON.TOKEN_NUMBER;
+					return TOKEN_STRING;
+				case '0':
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+				case '-':
+					return TOKEN_NUMBER;
 				case ':':
-					return JSON.TOKEN_COLON;
+					return TOKEN_COLON;
 			}
 			index--;
 
-			int remainingLength = json.Length - index;
+			var remainingLength = json.Length - index;
 
 			// false
-			if (remainingLength >= 5) {
+			if (remainingLength >= 5)
+			{
 				if (json[index] == 'f' &&
 					json[index + 1] == 'a' &&
 					json[index + 2] == 'l' &&
 					json[index + 3] == 's' &&
-					json[index + 4] == 'e') {
+					json[index + 4] == 'e')
+				{
 					index += 5;
-					return JSON.TOKEN_FALSE;
+					return TOKEN_FALSE;
 				}
 			}
 
 			// true
-			if (remainingLength >= 4) {
+			if (remainingLength >= 4)
+			{
 				if (json[index] == 't' &&
 					json[index + 1] == 'r' &&
 					json[index + 2] == 'u' &&
-					json[index + 3] == 'e') {
+					json[index + 3] == 'e')
+				{
 					index += 4;
-					return JSON.TOKEN_TRUE;
+					return TOKEN_TRUE;
 				}
 			}
 
 			// null
-			if (remainingLength >= 4) {
+			if (remainingLength >= 4)
+			{
 				if (json[index] == 'n' &&
 					json[index + 1] == 'u' &&
 					json[index + 2] == 'l' &&
-					json[index + 3] == 'l') {
+					json[index + 3] == 'l')
+				{
 					index += 4;
-					return JSON.TOKEN_NULL;
+					return TOKEN_NULL;
 				}
 			}
 
-			return JSON.TOKEN_NONE;
+			return TOKEN_NONE;
 		}
 
 		protected static bool SerializeValue(object value, StringBuilder builder)
 		{
-			bool success = true;
+			var success = true;
 
-			if (value is string) {
-				success = SerializeString((string)value, builder);
-			} else if (value is Dictionary<string,object>) {
-                success = SerializeObject((Dictionary<string, object>)value, builder);
-			} else if (value is List<object>) {
-                success = SerializeArray((List<object>)value, builder);
-			} else if (IsNumeric(value)) {
+			if (value is string)
+			{
+				success = SerializeString((string) value, builder);
+			}
+			else if (value is Dictionary<string, object>)
+			{
+				success = SerializeObject((Dictionary<string, object>) value, builder);
+			}
+			else if (value is List<object>)
+			{
+				success = SerializeArray((List<object>) value, builder);
+			}
+			else if (IsNumeric(value))
+			{
 				success = SerializeNumber(Convert.ToDouble(value), builder);
-			} else if ((value is Boolean) && ((Boolean)value == true)) {
+			}
+			else if ((value is Boolean) && (Boolean) value)
+			{
 				builder.Append("true");
-			} else if ((value is Boolean) && ((Boolean)value == false)) {
+			}
+			else if ((value is Boolean) && ((Boolean) value == false))
+			{
 				builder.Append("false");
-			} else if (value == null) {
+			}
+			else if (value == null)
+			{
 				builder.Append("null");
-			} else {
+			}
+			else
+			{
 				success = false;
 			}
 			return success;
 		}
 
-        protected static bool SerializeObject(
-            Dictionary<string, object> anObject, StringBuilder builder)
+		protected static bool SerializeObject(
+			Dictionary<string, object> anObject, StringBuilder builder)
 		{
 			builder.Append("{");
 
 			IDictionaryEnumerator e = anObject.GetEnumerator();
-			bool first = true;
-			while (e.MoveNext()) {
-				string key = e.Key.ToString();
-				object value = e.Value;
+			var first = true;
+			while (e.MoveNext())
+			{
+				var key = e.Key.ToString();
+				var value = e.Value;
 
-				if (!first) {
+				if (!first)
+				{
 					builder.Append(", ");
 				}
 
 				SerializeString(key, builder);
 				builder.Append(":");
-				if (!SerializeValue(value, builder)) {
+				if (!SerializeValue(value, builder))
+				{
 					return false;
 				}
 
@@ -403,19 +489,22 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 			return true;
 		}
 
-        protected static bool SerializeArray(List<object> anArray, StringBuilder builder)
+		protected static bool SerializeArray(List<object> anArray, StringBuilder builder)
 		{
 			builder.Append("[");
 
-			bool first = true;
-			for (int i = 0; i < anArray.Count; i++) {
-				object value = anArray[i];
+			var first = true;
+			for (var i = 0; i < anArray.Count; i++)
+			{
+				var value = anArray[i];
 
-				if (!first) {
+				if (!first)
+				{
 					builder.Append(", ");
 				}
 
-				if (!SerializeValue(value, builder)) {
+				if (!SerializeValue(value, builder))
+				{
 					return false;
 				}
 
@@ -430,28 +519,47 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 		{
 			builder.Append("\"");
 
-			char[] charArray = aString.ToCharArray();
-			for (int i = 0; i < charArray.Length; i++) {
-				char c = charArray[i];
-				if (c == '"') {
+			var charArray = aString.ToCharArray();
+			for (var i = 0; i < charArray.Length; i++)
+			{
+				var c = charArray[i];
+				if (c == '"')
+				{
 					builder.Append("\\\"");
-				} else if (c == '\\') {
+				}
+				else if (c == '\\')
+				{
 					builder.Append("\\\\");
-				} else if (c == '\b') {
+				}
+				else if (c == '\b')
+				{
 					builder.Append("\\b");
-				} else if (c == '\f') {
+				}
+				else if (c == '\f')
+				{
 					builder.Append("\\f");
-				} else if (c == '\n') {
+				}
+				else if (c == '\n')
+				{
 					builder.Append("\\n");
-				} else if (c == '\r') {
+				}
+				else if (c == '\r')
+				{
 					builder.Append("\\r");
-				} else if (c == '\t') {
+				}
+				else if (c == '\t')
+				{
 					builder.Append("\\t");
-				} else {
-					int codepoint = Convert.ToInt32(c);
-					if ((codepoint >= 32) && (codepoint <= 126)) {
+				}
+				else
+				{
+					var codepoint = Convert.ToInt32(c);
+					if ((codepoint >= 32) && (codepoint <= 126))
+					{
 						builder.Append(c);
-					} else {
+					}
+					else
+					{
 						builder.Append("\\u" + Convert.ToString(codepoint, 16).PadLeft(4, '0'));
 					}
 				}
@@ -468,10 +576,10 @@ namespace CocoB.Rest.WindowsPhone.Core.Parsers
 		}
 
 		/// <summary>
-		/// Determines if a given object is numeric in any way
-		/// (can be integer, double, null, etc). 
+		///   Determines if a given object is numeric in any way
+		///   (can be integer, double, null, etc). 
 		/// 
-		/// Thanks to mtighe for pointing out Double.TryParse to me.
+		///   Thanks to mtighe for pointing out Double.TryParse to me.
 		/// </summary>
 		protected static bool IsNumeric(object o)
 		{
